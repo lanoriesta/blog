6e3f277857a28ad8b54c20e634f635e73d470bc8
Lawrence Noriesta <lanoriesta@gmai.com>
2023-07-12 01:34:51 +0800

    update base BLOG x2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,7 +23,7 @@ const Header = () => {
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
               <li className="nav-item">
-                <NavLink className="nav-link text-light" to="/">
+                <NavLink className="nav-link text-light" to="BLOG">
                   Home
                 </NavLink>
               </li>
