d03746dd4381be2a16962a61ef8fa1b77d84eee2
Lawrence Noriesta <lanoriesta@gmai.com>
2023-07-19 21:19:39 +0800

    update link

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -5,7 +5,7 @@ const Error = () => {
   return (
     <div className="error-container">
       <h1>Page Not Found</h1>
-      <Link to="BLOG">Back to Homepage</Link>
+      <Link to="blog">Back to Homepage</Link>
     </div>
   );
 };
